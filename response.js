c2adcf627839ea8a8ad60e081f65ee313aab96e0
EyneQ <xslatter30@gmail.com>
2021-03-23 00:49:34 +1000

    extended the functionality of the response class

--- response.js
+++ response.js
@@ -1,3 +1,7 @@
+"use strict";
+
+let fs = require("fs");
+
 function extend(ServerResponse) {
 
     ServerResponse.prototype.statusCode = function(code) {
@@ -7,11 +11,23 @@ function extend(ServerResponse) {
 
     ServerResponse.prototype.json = function(json) {
         this.setHeader("Content-Type", "application/json");
-        this.write(JSON.stringify(json));
-        this.end();
-        return this;
+        return this.send(JSON.stringify(json));
     }
 
+    ServerResponse.prototype.send = function(data) {
+        return this.end(data);
+    }
+
+    ServerResponse.prototype.sendFile = function(path, callback) {
+        fs.readFile(path, function (error, data) {
+            if (error) {
+                this.statusCode(404);
+                callback(error);
+                return;
+            }
+            return this.send(data);       
+        });
+    }
 }
 
 module.exports = extend;
